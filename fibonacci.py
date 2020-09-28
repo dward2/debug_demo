c5b7bfcf95327b239f8aced14420f7ba46ea0fcd
David Ward <david.a.ward@duke.edu>
2020-09-28 10:21:38 -0400

    Revert "Add logging to fibinnaci sequence"
    This reverts commit ae4324a3

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,13 @@
-import logging
-import output_fib
-
 def fibinnaci(a, b, fib_list):
-    output_fib.output_fib()
     next_number = a + b
     fib_list.append(next_number)
-    logging.debug("fib_list is {}".format(fib_list))
     count = 0
     if next_number < 10:
         fibinnaci(b, next_number, fib_list)
         count += 1
     else:
-        logging.info("sequence ending")
-    return fib_list
+        print("sequence ending")
+        return fib_list
 
 
 def main():
@@ -22,6 +17,4 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="sequence.log", filemode='w',
-                        level=logging.DEBUG)
     main()
--- output_fib.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def output_fib():
-    print("Working on things")
-    logging.info("Working on thing")
